80527faba8a73ad7e385fa18a0993e5be519c3b8
Athul Sabu <116870115+Athulolikkal@users.noreply.github.com>
2023-04-03 10:55:28 +0530

    task completed issue fixed

--- src/Component/AddTask.jsx
+++ src/Component/AddTask.jsx
@@ -1,31 +1,42 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
+const AddTask = ({ addTask }) => {
+  const [value, setValue] = useState("");
 
-const AddTask = ({addTask}) => {
- 
-    const [value,setValue ]=useState('')
+  const addItem = () => {
+    addTask(value);
+    setValue("");
+  };
 
-  const addItem=()=>{
-   addTask(value)
-   setValue('');
- }
-   
-    return (
-  <>
-  
-  <div className='input-container '>
-       
-        <input type='text'  className='input ' placeholder='enter your task' value={value}  onChange={(e)=>{setValue(e.target.value)}} />
-       
-    
+  return (
+    <>
+      <div className="input-container ">
+        <input
+          type="text"
+          className="input "
+          placeholder="enter your task"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+
         <br />
-        {value?<i class="fa-sharp fa-solid fa-plus " style={{cursor:'pointer',height:'50px'}} onClick={addItem} ></i>:<i class="fa-sharp fa-solid fa-plus" style={{cursor:'pointer'}}></i>}
-         
-       
-    </div>
-
+        {value ? (
+          <i
+            class="fa-sharp fa-solid fa-plus "
+            style={{ cursor: "pointer", height: "50px" }}
+            onClick={addItem}
+          ></i>
+        ) : (
+          <i
+            class="fa-sharp fa-solid fa-plus"
+            style={{ cursor: "pointer" }}
+          ></i>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AddTask
+export default AddTask;
--- src/Component/ListTask.jsx
+++ src/Component/ListTask.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
+import React from "react";
 
-
-const ListTask = ({task,removeTask,taskCompleted}) => {
+const ListTask = ({ task, removeTask, taskCompleted }) => {
   return (
-   <>
-   <div className='list-Task'>
-   
-   <input className='checkbox' type="checkbox" name=''  style={{float:'left',cursor:'pointer'}} onChange={(e)=>{taskCompleted(e.target.checked,task.id)}} />
-        
-       {task.status?<p style={{display:'inline-block',textDecoration:'line-through',textDecorationColor:'white',opacity:'0.5'}}>{task.title}</p>:<p style={{display:'inline-block'}}>{task.title}</p>}
-       
-       {/* <i className="fa fa-trash delete-icon"  onClick={()=>{removeTask(index)}} ></i> */}
-       <i className="fa fa-trash delete-icon" style={{cursor:'pointer'}}  onClick={()=>{removeTask(task.id)}} ></i>
-      
-    
-       </div>
-      
-   </>
-    
-  )
-}
+    <>
+      <div className="list-Task">
+        <input
+          className="checkbox"
+          type="checkbox"
+          name=""
+          style={{ float: "left", cursor: "pointer" }}
+          onChange={(e) => {
+            taskCompleted(e.target.checked, task.id);
+          }}
+        />
 
-export default ListTask
+        {task.status ? (
+          <p
+            style={{
+              display: "inline-block",
+              textDecoration: "line-through",
+              textDecorationColor: "white",
+              opacity: "0.5",
+            }}
+          >
+            {task.title}
+          </p>
+        ) : (
+          <p style={{ display: "inline-block" }}>{task.title}</p>
+        )}
+
+        <i
+          className="fa fa-trash delete-icon"
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            removeTask(task.id);
+          }}
+        ></i>
+      </div>
+    </>
+  );
+};
+
+export default ListTask;
